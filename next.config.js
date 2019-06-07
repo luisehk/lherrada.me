f465c9b3bc19354310f9339435316014f5f4fa04
Luis Herrada <luisehk@gmail.com>
2019-06-07 16:53:36 -0500

    fix build

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   exportPathMap: function() {
     return {
       '/': { page: '/' },
-      '/blog': { page: '/blog' }
+      '/blog': { page: '/blog' },
       '/work': { page: '/work' },
       '/about': { page: '/about' }
     }
